537406afaa13bd5eb33715038805fdcace43bc55
Claus Aranha <caranha@gmail.com>
2016-12-21 00:47:17 +0900

    Work in progress on Uniform Decomposition (steps 2/4 complete)
    1- Generate non-factors of N
    2- Generate sets of N-1 non-factors
    3- Find Matrix U_N(h) with lowest DC
    4- Generate lambda weights from minimal U_N(h)

--- R/decomposition_uniform.R
+++ R/decomposition_uniform.R
@@ -18,4 +18,26 @@
 
 decomposition_uniform <- function(decomp, ...){
 
+  # Uniform requires that the number of objectives is at least 3
+  assertthat::assert_that(
+    assertthat::has_name(decomp, ".nobj"),
+    assertthat::is.count(decomp$.nobj),
+    decomp$.nobj >= 3)
+
+  N <- decomp$.nobj
+
+  # 1. Calculate the non-factors of N
+  div <- seq_len(N);
+  div <- div[N %% div != 0]
+
+  # 2. Generates a matrix of size |N-1| subsets of div
+  H <- t(sapply(seq_len(length(div)),
+                function(x) {return(div[seq_len(length(div)) != x])}
+        ))
+
+  # QUESTION: Do I need the permutation of these subsets as well, or just the subsets?
+
+  # 3. Generate matrixes U_N(h), and find one with lowest CD
+
+  # 4. Generate weights from U_N(h) with lowest CD
 }
