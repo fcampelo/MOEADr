ff3e6636747e6afa5cccb14f5733c2cd19556db6
fcampelo <fcampelo@ufmg.br>
2017-03-17 21:17:36 -0300

    Minor change in test_decomposition()

--- cran-comments.md
+++ cran-comments.md
@@ -1,3 +1,7 @@
+## Changes and notes
+* solved (hopefully) a Solaris-specific ERROR due to numeric types.
+* removed unnecessary dependency from Imports
+
 ## Test environments
 * local OS X install, R 3.3.1
 * ubuntu 12.04 (on travis-ci v 2.5.0), R 3.3.2
@@ -5,9 +9,4 @@
 
 ## R CMD check results
 0 errors | 0 warnings | 0 notes
-
 R CMD check succeeded
-
-## Changes
-* removed unnecessary dependency from Imports
-* attempted solution of a spurious, Solaris-specific NOTE
--- tests/testthat/test_decomposition.R
+++ tests/testthat/test_decomposition.R
@@ -23,19 +23,20 @@ decomp.sld2 <- list(name = "sld", H = 2)
 m2          <- 10
 
 testthat::test_that("SLD decomposition returns correct sizes", {
-  skip_on_os("solaris")
-  testthat::expect_equal(dim(generate_weights(decomp.sld1, m1)),
-                         c(choose(decomp.sld1$H + m1 - 1,
-                                  m1 - 1),
-                           m1))
-  testthat::expect_equal(dim(generate_weights(decomp.sld2, m2)),
-                         c(choose(decomp.sld2$H + m2 - 1,
-                                  m2 - 1),
-                           m2))
+
+  testthat::expect_true(correct_dimensions(generate_weights(decomp.sld1, m1),
+                                           rows = choose(decomp.sld1$H + m1 - 1,
+                                                         m1 - 1),
+                                           cols = m1))
+
+  testthat::expect_true(correct_dimensions(generate_weights(decomp.sld2, m2),
+                                           rows = choose(decomp.sld2$H + m2 - 1,
+                                                         m2 - 1),
+                                           cols = m2))
 })
 
 testthat::test_that("SLD decomposition returns unitary weight vectors", {
-  skip_on_os("solaris")
+
   testthat::expect_true(rows_sum_one(generate_weights(decomp.sld1, m1)))
   testthat::expect_true(rows_sum_one(generate_weights(decomp.sld2, m2)))
 })
@@ -51,22 +52,18 @@ res3           <- cbind(Var1=c(0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.35, 0.45, 0
                         VarLast=c(0.75, 0.65, 0.55, 0.45, 0.35, 0.25, 0.65, 0.55, 0.45, 0.35, 0.60, 0.50, 0.40))
 
 testthat::test_that("MSDL rejects invalid inputs", {
-  skip_on_os("solaris")
   testthat::expect_error(generate_weights(decomp.msdl1, msld1))
 })
 testthat::test_that("MSDL returns correct size", {
-  skip_on_os("solaris")
   testthat::expect_equal(dim(generate_weights(decomp.msld2, msld2)),
                          c(34,4))
 })
 testthat::test_that("MSDL returns correct values", {
-  skip_on_os("solaris")
   testthat::expect_equal(generate_weights(decomp.msld3, msld3),
                          res3)
 })
 
 testthat::test_that("MSLD returns unitary weight vectors", {
-  skip_on_os("solaris")
   testthat::expect_true(rows_sum_one(generate_weights(decomp.msld2, msld2)))
   testthat::expect_true(rows_sum_one(generate_weights(decomp.msld3, msld3)))
 })
@@ -78,7 +75,6 @@ decomp.unif3 <- list(name = "uniform", N = 32)
 unif_obj <- 4
 
 testthat::test_that("Uniform returns unitary weight vectors", {
-  skip_on_os("solaris")
   testthat::expect_true(rows_sum_one(generate_weights(decomp.unif1, unif_obj)))
   # testthat::expect_true(is_valid_decomposition(generate_weights(decomp.unif2, unif_obj)))
   testthat::expect_true(rows_sum_one(generate_weights(decomp.unif3, unif_obj)))
@@ -86,13 +82,11 @@ testthat::test_that("Uniform returns unitary weight vectors", {
 
 # Issue 34 on decomposition - Wrong matrix dimensions when number of subobjectives is 2
 testthat::test_that("Uniform returns correct dimensions", {
-  skip_on_os("solaris")
   testthat::expect_true(correct_dimensions(generate_weights(list(name = "uniform", N = 10), 3), 10, 3))
   testthat::expect_true(correct_dimensions(generate_weights(list(name = "uniform", N = 10), 2), 10, 2))
 })
 
 testthat::test_that("Uniform return matrix sum to one", {
-  skip_on_os("solaris")
   testthat::expect_true(rows_sum_one(generate_weights(list(name = "uniform", N = 10), 3)))
   testthat::expect_true(rows_sum_one(generate_weights(list(name = "uniform", N = 10), 2)))
 })
