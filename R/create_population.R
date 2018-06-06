3444eb9acaaf6a8240503596317abeb0dcf3bb34
yclavinas <yclavinas@gmail.com>
2018-06-06 15:12:33 +0900

    cga done

--- R/create_population.R
+++ R/create_population.R
@@ -22,7 +22,8 @@
 #' @export
 
 create_population <- function(N,       # population size
-                              problem) # list of named problem parameters
+                              problem, # list of named problem parameters
+                              increments = NULL)
 {
 
     # ========== Error catching and default value definitions
