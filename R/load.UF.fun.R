906b2cbacb9be81e091cd13e1534258e93b13803
yclavinas <yclavinas@gmail.com>
2018-11-05 19:22:44 +0900

    uf

--- R/load.UF.fun.R
+++ R/load.UF.fun.R
@@ -1,86 +1,31 @@
-load.UF.function <- function(fun, n.obj){
-  if (fun == "UF1"){
-    UF1 = generateUF(1, in.dim = d, out.dim = n.obj)
-    pf = getParetoFront(UF1, 10000)
-    problem.UF <- function(X) {
-      t(apply(X, MARGIN = 1,
-              FUN = UF1
-      ))
-    }
-  }else if (fun == "UF2"){
-    UF2 = generateUF(2, in.dim = d, out.dim = n.obj)
-    pf = getParetoFront(UF2, 10000)
-    problem.UF <- function(X) {
-      t(apply(X, MARGIN = 1,
-              FUN = UF2
-      ))
-    }
-  }else if (fun == "UF3"){
-    UF3 = generateUF(3, in.dim = d, out.dim = n.obj)
-    pf = getParetoFront(UF3, 10000)
-    problem.UF <- function(X) {
-      t(apply(X, MARGIN = 1,
-              FUN = UF3
-      ))
-    }
-  }else if (fun == "UF4"){
-    UF4 = generateUF(4, in.dim = d, out.dim = n.obj)
-    pf = getParetoFront(UF4, 10000)
-    problem.UF <- function(X) {
-      t(apply(X, MARGIN = 1,
-              FUN = UF4
-      ))
-    }
-  }else if (fun == "UF5"){
-    UF5 = generateUF(5, in.dim = d, out.dim = n.obj)
-    pf = getParetoFront(UF5, 10000)
-    problem.UF <- function(X) {
-      t(apply(X, MARGIN = 1,
-              FUN = UF5
-      ))
-    }
-  }else if (fun == "UF6"){
-    UF6 = generateUF(6, in.dim = d, out.dim = n.obj)
-    pf = getParetoFront(UF6, 10000)
-    problem.UF <- function(X) {
-      t(apply(X, MARGIN = 1,
-              FUN = UF6
-      ))
-    }
-  }else if (fun == "UF7"){
-    UF7 = generateUF(7, in.dim = d, out.dim = n.obj)
-    pf = getParetoFront(UF7, 10000)
-    problem.UF <- function(X) {
-      t(apply(X, MARGIN = 1,
-              FUN = UF7
-      ))
-    }
+load.UF.function <- function(fun, d){
+  if (fun == "UF1") {
+    uf1 = makeUFFunction(id = 1,dimensions = d)
+    problem.uf = uf1
   }
-  else if (fun == "UF8"){
-    UF8 = generateUF(8, in.dim = d, out.dim = n.obj)
-    pf = getParetoFront(UF8, 10000)
-    problem.UF <- function(X) {
-      t(apply(X, MARGIN = 1,
-              FUN = UF8
-      ))
-    }
-  }else if (fun == "UF9"){
-    UF9 = generateUF(9, in.dim = d, out.dim = n.obj)
-    pf = getParetoFront(UF9, 10000)
-    problem.UF <- function(X) {
-      t(apply(X, MARGIN = 1,
-              FUN = UF9
-      ))
-    }
-    }else if (fun == "UF10"){
-    UF10 = generateUF(10, in.dim = d, out.dim = n.obj)
-    pf = getParetoFront(UF10, 10000)
-    problem.UF <- function(X) {
-      t(apply(X, MARGIN = 1,
-              FUN = UF10
-      ))
-    }
+  else if (fun == "UF2") {
+    uf2 = makeUFFunction(id = 2,dimensions = d)
+    problem.uf = uf2
+  }else if (fun == "F3") {
+    uf3 = makeUFFunction(id = 3,dimensions = d)
+    problem.uf = uf3
   }
-  out <- list(problem = problem.UF, pf = pf)
+  else if (fun == "UF4") {
+    uf4 = makeUFFunction(id = 4,dimensions = d)
+    problem.uf = uf4
+  }
+  else if (fun == "UF5") {
+    uf5 = makeUFFunction(id = 5,dimensions = d)
+    problem.uf = uf5
+  }
+  else if (fun == "UF") {
+    uf6 = makeUFFunction(id = 6,dimensions = d)
+    problem.uf = uf6
+  }
+  else if (fun == "UF7") {
+    uf7 = makeUFFunction(id = 7,dimensions = d)
+    problem.uf = uf7
+  }
+  out <- list(fn = problem.uf)
   return(out)
 }
