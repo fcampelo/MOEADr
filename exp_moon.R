c07a782e8309bf9ee62d08aca719198ed05fd9c8
yclavinas <yclavinas@gmail.com>
2018-11-15 08:25:47 +0900

    era o poly nao o de

--- exp_moon.R
+++ exp_moon.R
@@ -18,23 +18,25 @@ resource.allocation.GRA <- list(name = "GRA", dt = 20)
 resource.allocation.RAD <- list(name = "RAD", dt = 20)
 
 
-decomp <- list(name = "SLD", H = 20)
-decomp2 <- list(name = "uniform", N = 150)
+decomp <- list(name = "SLD", H =16)
+decomp2 <- list(name = "uniform", N = 300)
 
 scaling <- list()
 scaling$name <- "simple"
 
 variation <- preset_moead("moead.de")$variation
 # variation[[4]] <- variation[[3]]
-variation[[1]] <-
-  list(name = "de")
+# variation[[1]] <-
+#   list(name = "de")
 variation[[4]] <- list(name = "localsearch", type = "tpqa", gamma.ls = 0.5)
 
 update <- preset_moead("moead.de")$update
 update$UseArchive = TRUE
+# update$UseArchive = FALSE
+# update$nsga = TRUE
 update$nsga = FALSE
-
-
+update2 <- list(name  = "onra")
+update2$UseArchive = TRUE
 n.obj <- 3
 
 
@@ -57,10 +59,6 @@ for (algo in algorithms) {
   )
   ref.points <- matrix(c(1.0, 0.0, 1.0), nrow = 1, ncol = 3)
   for (j in 1:repetitions) {
-    # moead.de.data <- list()
-    # # moead.dra.data <- list()
-    # moead.gra.data <- list()
-    # moead.rad.data <- list()
     
     cat("rep:", j)
     
@@ -76,28 +74,29 @@ for (algo in algorithms) {
       decomp = decomp,
       stopcrit = stopcrit,
       update = update,
-      constraint = list(name = "penalty", beta=0.05),
+      variation = variation,
+      constraint = list(name = "penalty", beta=0.95),
       showpars = list(show.iters = "none", showevery = 100),
       seed = j,
       my.file.n = my.file.n
     )
-    # # #
-    # # # # gra.awt
-    # my.file.n <- paste0("gra/",with_options(
-    #   c(scipen = 999),
-    #   str_pad(j - 1, 3, pad = "0")
-    # ))
-    # moead.gra <- moead(
-    #   problem  = problem.zdt1,
-    #   preset   = preset_moead(algo),
-    #   decomp = decomp,
-    #   update = update,
-    #   stopcrit = stopcrit,
-    #   constraint = list(name = "penalty", beta=0.0),
-    #   showpars = list(show.iters = "none", showevery = 100),
-    #   seed = j,
-    #   my.file.n = my.file.n
-    # )
+    # gra.awt
+    my.file.n <- paste0("gra/",with_options(
+      c(scipen = 999),
+      str_pad(j - 1, 3, pad = "0")
+    ))
+    moead.gra <- moead(
+      problem  = problem.zdt1,
+      preset   = preset_moead("original"),
+      decomp = decomp,
+      stopcrit = stopcrit,
+      update = update,
+      variation = variation,
+      constraint = list(name = "penalty", beta=0.95),
+      showpars = list(show.iters = "none", showevery = 100),
+      seed = j,
+      my.file.n = my.file.n
+    )
 
     # # ondb
     my.file.n <- paste0("rad/",with_options(
@@ -111,29 +110,17 @@ for (algo in algorithms) {
       stopcrit = stopcrit,
       update = update,
       variation = variation,
-      constraint = list(name = "penalty", beta=0.05),
-      showpars = list(show.iters = "none", showevery = 10),
+      constraint = list(name = "penalty", beta=0.95),
+      showpars = list(show.iters = "none", showevery = 100),
       seed = j,
       resource.allocation = resource.allocation.RAD,
       my.file.n = my.file.n
     )
     print("moead.rad$n.iter")
     print(moead.rad$n.iter)
-    moead.rad.non.d <- find_nondominated_points(moead.rad$Archive$Y)
-    moead.rad.hv <-
-      emoa::dominated_hypervolume(points = t(moead.rad$Archive$Y[moead.rad.non.d, ]),
-                                  ref = ref.points)
-    print(moead.rad.hv)
-    print("moead.de")
-    # print(moead.de$n.iter)
-    moead.de.non.d <- find_nondominated_points(moead.de$Archive$Y)
-    moead.de.hv <-
-      emoa::dominated_hypervolume(points = t(moead.de$Archive$Y[moead.de.non.d, ]),
-                                  ref = ref.points)
-    print(moead.de.hv)
-    # moead.de.non.d <- find_nondominated_points(moead.gra$Y)
+    # moead.de.non.d <- find_nondominated_points(moead.de$Archive$Y)
     # moead.de.hv <-
-    #   emoa::dominated_hypervolume(points = t(moead.gra$Y[moead.de.non.d, ]),
+    #   emoa::dominated_hypervolume(points = t(moead.de$Archive$Y[moead.de.non.d, ]),
     #                               ref = ref.points)
     # print(moead.de.hv)
   }
