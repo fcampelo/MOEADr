d21b931353b0b72d0fff64b1db5082e072679474
yclavinas <yclavinas@gmail.com>
2018-11-17 12:04:08 +0900

    30 rep

--- exp_UF.R
+++ exp_UF.R
@@ -27,8 +27,8 @@ scaling$name <- "simple"
 
 variation <- preset_moead("moead.de")$variation
 # variation[[4]] <- variation[[3]]
-variation[[1]] <-
-  list(name = "de")
+# variation[[1]] <-
+#   list(name = "de")
 
 update <- preset_moead("moead.de")$update
 update$UseArchive = TRUE
