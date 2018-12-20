54577a29a404a1b662795a9e92438609581dd80a
yclavinas <yclavinas@gmail.com>
2018-12-20 18:48:40 +0900

    ahhhh

--- exp_UF.R
+++ exp_UF.R
@@ -115,44 +115,44 @@ for (n.obj in n.objs) {
         # write_feather(as.data.frame(moead.gra$Y), my.file.n)
         
         # ondb
-        # my.file.n <- paste0("../../rad/",fun,"_")
-        # moead.rad <- moead(
-        #   problem  = problem.zdt1,
-        #   preset   = preset_moead(algo),
-        #   decomp = decomp,
-        #   stopcrit = stopcrit,
-        #   scaling = scaling,
-        #   showpars = list(show.iters = "none", showevery = 10),
-        #   seed = j,
-        #   resource.allocation = resource.allocation.RAD,
-        #   my.file.n = my.file.n
-        # )
-        # 
-        # my.file.n <- paste0("../../norm/",fun,"_")
-        # moead.norm <- moead(
-        #   problem  = problem.zdt1,
-        #   preset   = preset_moead(algo),
-        #   decomp = decomp,
-        #   stopcrit = stopcrit,
-        #   scaling = scaling,
-        #   showpars = list(show.iters = "none", showevery = 10),
-        #   seed = j,
-        #   resource.allocation = resource.allocation.NORM,
-        #   my.file.n = my.file.n
-        # )
-        
-        # my.file.n <- paste0("../../random/",fun,"_")
-        # moead.random <- moead(
-        #   problem  = problem.zdt1,
-        #   preset   = preset_moead(algo),
-        #   decomp = decomp,
-        #   stopcrit = stopcrit,
-        #   scaling = scaling,
-        #   showpars = list(show.iters = "none", showevery = 10),
-        #   seed = j,
-        #   resource.allocation = resource.allocation.RANDOM,
-        #   my.file.n = my.file.n
-        # )
+        my.file.n <- paste0("../../rad/",fun,"_")
+        moead.rad <- moead(
+          problem  = problem.zdt1,
+          preset   = preset_moead(algo),
+          decomp = decomp,
+          stopcrit = stopcrit,
+          scaling = scaling,
+          showpars = list(show.iters = "none", showevery = 10),
+          seed = j,
+          resource.allocation = resource.allocation.RAD,
+          my.file.n = my.file.n
+        )
+        #
+        my.file.n <- paste0("../../norm/",fun,"_")
+        moead.norm <- moead(
+          problem  = problem.zdt1,
+          preset   = preset_moead(algo),
+          decomp = decomp,
+          stopcrit = stopcrit,
+          scaling = scaling,
+          showpars = list(show.iters = "none", showevery = 10),
+          seed = j,
+          resource.allocation = resource.allocation.NORM,
+          my.file.n = my.file.n
+        )
+
+        my.file.n <- paste0("../../random/",fun,"_")
+        moead.random <- moead(
+          problem  = problem.zdt1,
+          preset   = preset_moead(algo),
+          decomp = decomp,
+          stopcrit = stopcrit,
+          scaling = scaling,
+          showpars = list(show.iters = "none", showevery = 10),
+          seed = j,
+          resource.allocation = resource.allocation.RANDOM,
+          my.file.n = my.file.n
+        )
         # exit()
         # 
         # 
