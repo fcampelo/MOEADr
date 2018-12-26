ddd4b80d2b8a0aa2596fcdfd99160ff4d1314c56
yclavinas <yclavinas@gmail.com>
2018-12-26 10:19:03 +0900

    test

--- exp_moon.R
+++ exp_moon.R
@@ -3,51 +3,40 @@ setwd("~/MOEADr/R/")
 library(smoof)
 library(MOEADr)
 library(emoa)
-library(ecr)
 library(stringr)
+library(ecr)
+library(mco)
+library(feather)
+library(pracma)
 library(withr)
 lapply(list.files(pattern = "[.]R$", recursive = TRUE), source)
 
 repetitions <-  21
-
+dimension <- 30
 algorithms <- c("moead.de")
 
 #uniform weight
-resource.allocation.DRA <- list(name = "DRA", dt = 50)
+resource.allocation.DRA <- list(name = "DRA", dt = 20)
 resource.allocation.GRA <- list(name = "GRA", dt = 20)
-resource.allocation.RAD <- list(name = "RAD", dt = 50)
+resource.allocation.RAD <- list(name = "RAD", dt = 20)
+resource.allocation.NORM <- list(name = "norm", dt = 20)
+resource.allocation.RANDOM <- list(name = "random", dt = 20)
 
 
 
-decomp <- list(name = "SLD", H = 14)
-
-preset <- preset_moead("moead.de")
-preset$decomp <- decomp
-preset$aggfun$name <- "awt"
-preset$neighbors$name <- "lambda"
-preset$neighbors$T <- 20
-# preset$neighbors$T <- 14
-preset$neighbors$delta.p <- 0.5
-preset$variation[[1]]$phi   <- NULL
-preset$variation[[2]]$pm   <- 0.445
-truncation <- preset$variation[[3]]
-
-
-update <- preset_moead("moead.de")$update
-update$UseArchive = TRUE
-update$nsga = TRUE
-update$ws_transformation = FALSE
-
-preset$update <- update
+decomp <- list(name = "SLD", H = 25)
 
 n.obj <- 3
 
 
 stopcrit  <- list(list(name    = "maxeval",
-                       maxeval = 30000))
+                       maxeval = 140000))
+
+scaling <- list()
+scaling$name <- "simple"
 
 d <- 2
-
+fun <- "moon"
 my.data <- data.frame()
 for (algo in algorithms) {
   # print(algo)
@@ -66,68 +55,78 @@ for (algo in algorithms) {
     cat("rep:", j)
     
     
-    # moead.de
-    # print("moead.de")
-    # my.file.n <- paste0("de/",with_options(
-    #  c(scipen = 999),
-    #  str_pad(j - 1, 3, pad = "0")
-    # ))
-    # moead.de <- moead(
+    my.file.n <- paste0("../../de/",fun,"_")
+    moead.de <- moead(
+      problem  = problem.zdt1,
+      preset   = preset_moead(algo),
+      decomp = decomp,
+      stopcrit = stopcrit,
+      constraint = list(name = "penalty", beta=0.95),
+      scaling = scaling,
+      showpars = list(show.iters = "none", showevery = 100),
+      seed = j,
+      my.file.n = my.file.n
+    )
+    
+    
+    # gra.awt
+    my.file.n <- paste0("../../gra/",fun,"_")
+    moead.gra <- moead(
+      problem  = problem.zdt1,
+      preset   = preset_moead(algo),
+      decomp = decomp,
+      stopcrit = stopcrit,
+      constraint = list(name = "penalty", beta=0.95),
+      scaling = scaling,
+      showpars = list(show.iters = "none", showevery = 100),
+      seed = j,
+      resource.allocation = resource.allocation.GRA,
+      my.file.n = my.file.n
+    )
+    
+    # write_feather(as.data.frame(moead.gra$Y), my.file.n)
+    
+    # ondb
+    # my.file.n <- paste0("../../rad/",fun,"_")
+    # moead.rad <- moead(
     #   problem  = problem.zdt1,
     #   preset   = preset_moead(algo),
     #   decomp = decomp,
     #   stopcrit = stopcrit,
-    #   update = update2,
-    #   variation = variation,
-    #   constraint = list(name = "penalty", beta=0.95),
-    #   showpars = list(show.iters = "none", showevery = 100),
+    #   scaling = scaling,
+    #   showpars = list(show.iters = "none", showevery = 10),
     #   seed = j,
+    #   resource.allocation = resource.allocation.RAD,
     #   my.file.n = my.file.n
     # )
-    my.file.n <- paste0("de/",with_options(
-      c(scipen = 999),
-      str_pad(j - 1, 3, pad = "0")
-    ))
-    moead.de <- moead(
-     problem  = problem.zdt1,
-     preset   = preset,
-     stopcrit = stopcrit,
-     constraint = list(name = "penalty", beta=0.95),
-     showpars = list(show.iters = "none", showevery = 100),
-     seed = j,
-     my.file.n = my.file.n
-    )
-    # 
-    # # # ondb
-    my.file.n <- paste0("rad/",with_options(
-      c(scipen = 999),
-      str_pad(j - 1, 3, pad = "0")
-    ))
-    # print("moead.rad")
-    moead.rad <- moead(
+    #
+    my.file.n <- paste0("../../norm/",fun,"_")
+    moead.norm <- moead(
       problem  = problem.zdt1,
-      preset   = preset,
+      preset   = preset_moead(algo),
+      decomp = decomp,
       stopcrit = stopcrit,
       constraint = list(name = "penalty", beta=0.95),
-      showpars = list(show.iters = "none", showevery = 100),
+      scaling = scaling,
+      showpars = list(show.iters = "none", showevery = 10),
       seed = j,
-      resource.allocation = resource.allocation.RAD,
+      resource.allocation = resource.allocation.NORM,
       my.file.n = my.file.n
     )
-    print("moead.rad$n.iter")
-    print(moead.rad$n.iter)
-    if (j %% 4==0) cat("\n")
     
-    # moead.de.non.d <- find_nondominated_points(moead.de$Archive2$Y)
-    # moead.de.hv <-
-    #   emoa::dominated_hypervolume(points = t(moead.de$Archive2$Y),
-    #                               ref = ref.points)
-    # print(moead.de.hv)
-    # moead.rad.non.d <- find_nondominated_points(moead.rad$Archive2$Y)
-    # moead.rad.hv <-
-    #   emoa::dominated_hypervolume(points = t(moead.rad$Archive2$Y),
-    #                               ref = ref.points)
-    # print(moead.rad.hv)
+    my.file.n <- paste0("../../random/",fun,"_")
+    moead.random <- moead(
+      problem  = problem.zdt1,
+      preset   = preset_moead(algo),
+      decomp = decomp,
+      stopcrit = stopcrit,
+      constraint = list(name = "penalty", beta=0.95),
+      scaling = scaling,
+      showpars = list(show.iters = "none", showevery = 10),
+      seed = j,
+      resource.allocation = resource.allocation.RANDOM,
+      my.file.n = my.file.n
+    )
   
   }
 }
