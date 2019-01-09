dd19c644819e74f8af47933069867857edba14d5
yclavinas <yclavinas@gmail.com>
2019-01-09 15:02:23 +0900

    igd correction

--- create_data_moon.R
+++ create_data_moon.R
@@ -62,7 +62,7 @@ for (fun in fun.names) {
           fun = fun,
           runIdPre = runIdPre,
           iRun = iRun,
-          gen = gen,
+          my.gen = gen,
           flag = 0
         )$my.data2[, 1:2]
       max.val <- pmax(max.val, apply(my.data, 2, max))
@@ -121,7 +121,7 @@ for (fun in fun.names) {
           fun = fun,
           runIdPre = runIdPre,
           iRun = iRun,
-          gen = gen,
+          my.gen = gen,
           flag = 2
         )
       my.data <- out$my.data2
@@ -315,8 +315,8 @@ for (fun in fun.names) {
   for (variant in variants) {
     none.median <-
       df[which(df[df$algorithm == variant, ]$HV == median(df[df$algorithm == variant, ]$HV)), ]
-    
-    print(none.median$rep)
+    print(variant)
+    cat(": ",none.median$rep)
     if (variant == "None") name = "de"
     else if (variant == "MRDL") name = "rad"
     else if (variant == "R.I.") name = "gra"
@@ -336,7 +336,7 @@ for (fun in fun.names) {
         fun = fun,
         runIdPre = runIdPre,
         iRun = iRun,
-        gen = gen,
+        my.gen = gen,
         flag = 1
       )
     # plot(out$my.data2[,1:2])
