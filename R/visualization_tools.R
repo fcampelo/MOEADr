e66ebaa4667a3c14385ff28cc33039e42c60606f
yclavinas <yclavinas@biman.mma.cs.tsukuba.ac.jp>
2019-06-14 17:21:24 +0900

    data DTLZ

--- R/visualization_tools.R
+++ R/visualization_tools.R
@@ -76,42 +76,42 @@ iteration.data$stage <- unlist(iteration.data$stage)
 # iteration.data$f1 <- (iteration.data$f1 - min(iteration.data$f1))/(max(iteration.data$f1) - min(iteration.data$f1))
 # iteration.data$f2 <- (iteration.data$f2 - min(iteration.data$f2))/(max(iteration.data$f2) - min(iteration.data$f2))
 
-df.pca <- data.frame()
+#df.pca <- data.frame()
 iteration.data$stage <- unlist(iteration.data$stage)
 # Yref <-
 #   as.matrix(read.table(paste0("inst/extdata/pf_data/DTLZ4.2D.pf")))
 
 hvs <- list(rep(0, max(iteration.data$stage)))
-igds <- list(rep(0, max(iteration.data$stage)))
+#igds <- list(rep(0, max(iteration.data$stage)))
 for (i in 1:max(iteration.data$stage)){
   Y <- rbind(iteration.data[iteration.data$stage==i,]$f1, iteration.data[iteration.data$stage==i,]$f2)
   hvs[[i]] <- emoa::dominated_hypervolume(points = Y, ref = rep(1.1,dim(moea$W)[2]))
 }
 
 
-for (i in 1:max(iteration.data$stage)){
-  if (!is.null(ref.front)){
-    Y <- rbind(iteration.data[iteration.data$stage==i,]$f1, iteration.data[iteration.data$stage==i,]$f2)
-    igds[[i]] <- igd <- calcIGD(Y, Yref = ref.front)
-  }else{
-    igds[[i]] <- 0
-  }
-}
+#for (i in 1:max(iteration.data$stage)){
+#  if (!is.null(ref.front)){
+#    Y <- rbind(iteration.data[iteration.data$stage==i,]$f1, iteration.data[iteration.data$stage==i,]$f2)
+#    igds[[i]] <- igd <- calcIGD(Y, Yref = ref.front)
+#  }else{
+#    igds[[i]] <- 0
+#  }
+#}
 
 
-for (i in 1:max(iteration.data$stage)){
-  temp <- iteration.data[iteration.data$stage==i,]
-  temp <- temp[ ,(length(iteration.data)-length(pareto.set)+2):length(iteration.data)-1]
-  paretoset <- matrix(unlist(temp[,-ncol(temp)]), ncol = ncol(temp)-1, nrow = nrow(temp))
-  paretoset.pca <- prcomp(paretoset, center = TRUE, scale = TRUE)
-  df.pca <- rbind(df.pca, data.frame(i, paretoset.pca$x[,1], paretoset.pca$x[,3]))
+#for (i in 1:max(iteration.data$stage)){
+#  temp <- iteration.data[iteration.data$stage==i,]
+#  temp <- temp[ ,(length(iteration.data)-length(pareto.set)+2):length(iteration.data)-1]
+#  paretoset <- matrix(unlist(temp[,-ncol(temp)]), ncol = ncol(temp)-1, nrow = nrow(temp))
+#  paretoset.pca <- prcomp(paretoset, center = TRUE, scale = TRUE)
+#  df.pca <- rbind(df.pca, data.frame(i, paretoset.pca$x[,1], paretoset.pca$x[,3]))
 
-}
+#}
 
-names(df.pca) <- c("stage", "Comp.1", "Comp.2")
-df.pca$Nondominated <- as.numeric(iteration.data$non.dominated)
-df.pca$Resources <- as.numeric(iteration.data$Resources)
-df.pca$Subproblem <- as.numeric(iteration.data$Subproblem)
+#names(df.pca) <- c("stage", "Comp.1", "Comp.2")
+#df.pca$Nondominated <- as.numeric(iteration.data$non.dominated)
+#df.pca$Resources <- as.numeric(iteration.data$Resources)
+#df.pca$Subproblem <- as.numeric(iteration.data$Subproblem)
 
 hvs <- data.frame(unlist(hvs), 1:max(iteration.data$stage))
 names(hvs) <- c("hv", "stage") 
@@ -120,11 +120,11 @@ hvs2 <- hvs %>%
 hvs2$stage <- hvs2$frame
 
 
-igds <- data.frame(unlist(igds), 1:max(iteration.data$stage))
-names(igds) <- c("igd", "stage")
-igds2 <- igds %>%
-  accumulate_by(~stage)
-igds2$stage <- igds2$frame
+#igds <- data.frame(unlist(igds), 1:max(iteration.data$stage))
+#names(igds) <- c("igd", "stage")
+#igds2 <- igds %>%
+#  accumulate_by(~stage)
+#igds2$stage <- igds2$frame
 
 # resource/subproblem with size by by nondom
 resource.problem <- iteration.data %>%
@@ -184,21 +184,21 @@ pf.resource <- iteration.data %>%
   ) 
 
 # pareto set
-ps.pca <- df.pca %>%
-  plot_ly(
-    x = ~ Comp.1,
-    y = ~ Comp.2,
-    frame = ~ stage,
-    type = 'scatter',
-    mode = 'markers',
-    showlegend = F,
-    colors = c('black', 'blue'),
-    color = ~ Nondominated,
-    alpha=0.5,
-    marker = list(size = ~ log(Resources+1)*5, opacity = 0.5),
-    text = ~ paste("Nondominated: ", Nondominated, '<br>Subproblem:', Subproblem),
-    hoverinfo = 'text'
-  )
+#ps.pca <- df.pca %>%
+#  plot_ly(
+#    x = ~ Comp.1,
+#    y = ~ Comp.2,
+#    frame = ~ stage,
+#    type = 'scatter',
+#    mode = 'markers',
+#    showlegend = F,
+#    colors = c('black', 'blue'),
+#    color = ~ Nondominated,
+#    alpha=0.5,
+#    marker = list(size = ~ log(Resources+1)*5, opacity = 0.5),
+ #   text = ~ paste("Nondominated: ", Nondominated, '<br>Subproblem:', Subproblem),
+ #   hoverinfo = 'text'
+ # )
 
 
 
@@ -235,33 +235,33 @@ hv.plot <- hvs2 %>%
   ) 
 
 
-igd.plot <- igds2 %>%
-  plot_ly(
-    y = ~ igd,
-    frame = ~ stage,
-    type = 'scatter',
-    mode = 'lines',
-    showlegend = F,
-    colors = c('black', 'blue'),
-    fill = 'tozeroy',
-    fillcolor='rgba(58, 83, 155, 0.5)',
-    line = list(color = 'rgba(58, 83, 155, 1)')
-    ) %>%
-  layout(
-    xaxis = list(
-      title = "Day",
-      range = c(0,max(hvs2$stage)),
-      zeroline = F,
-      showgrid = F
-    )
-  ) %>%
-  animation_opts(
-    frame = 100,
-    transition = 0,
-    redraw = FALSE
-  )
+#igd.plot <- igds2 %>%
+#  plot_ly(
+#    y = ~ igd,
+#    frame = ~ stage,
+#    type = 'scatter',
+#    mode = 'lines',
+#    showlegend = F,
+#    colors = c('black', 'blue'),
+#    fill = 'tozeroy',
+#    fillcolor='rgba(58, 83, 155, 0.5)',
+#    line = list(color = 'rgba(58, 83, 155, 1)')
+#    ) %>%
+#  layout(
+#    xaxis = list(
+#      title = "Day",
+#      range = c(0,max(hvs2$stage)),
+#      zeroline = F,
+ #     showgrid = F
+#    )
+#  ) %>%
+#  animation_opts(
+#    frame = 100,
+#    transition = 0,
+#    redraw = FALSE
+#  )
 
-p <- subplot(resource.problem, pf.resource, pf.nondominated, hv.plot, igd.plot, ps.pca, nrows = 2)%>%
+p <- subplot(resource.problem, pf.resource, pf.nondominated, hv.plot,  nrows = 2)%>%
   animation_slider(
     currentvalue = list(
       prefix = "Iteration "
@@ -360,7 +360,7 @@ loadPlotData <- function (name, j){
     nfe         = nfe,
     n.iter      = iter,
     time        = time,
-    Vmatrix = Vmatrix,
+    #Vmatrix = Vmatrix,
     plot.paretofront = plot.paretofront,
     plot.paretoset = plot.paretoset,
     plot.resources = plot.resources,
