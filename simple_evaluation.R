bd24a38a61fc801bb875b65f00e0ad0bea443454
yclavinas <yclavinas@gmail.com>
2019-06-26 16:36:46 +0900

    finally saving

--- simple_evaluation.R
+++ simple_evaluation.R
@@ -8,8 +8,8 @@ library(withr)
 lapply(list.files(pattern = "[.]R$", recursive = TRUE), source)
 fun.names1 <- list()
 source("visualization_tools.R")
-number.fun <- 7
-repetitions <- 21
+number.fun <- 1
+repetitions <- 1
 
 for (i in 1:number.fun) {
   fun.names1[[length(fun.names1) + 1]] = paste0("DTLZ", i)
@@ -55,7 +55,7 @@ for (fun in fun.names1) {
     class(moead.norm) <- "moead"
     class(moead.norm.inverse) <- "moead"
     class(moead.random) <- "moead"
-    class(moead.R.I) <- "moead"
+    class(moead.R.I.) <- "moead"
     
     Yref <-
       as.matrix(read.table(paste0(
@@ -108,7 +108,7 @@ for (fun in fun.names1) {
 }
 print(aggregate(results$hv, median, by = list(results$name, results$fun)))
 print(aggregate(results$igd, median, by = list(results$name, results$fun)))
-write_feather(results, "results")
+write_feather(results, "results_dtlz")
 
 ####
 for (fun in fun.names1) {
@@ -146,17 +146,23 @@ moead.R.I. <-
 
 
 p <- visuEvol(moead.de, "MOEA/D-DE - No Resource Allocation", fun, ref1)
-htmlwidgets::saveWidget(p, file = paste0("~/MOEADr/dataExp/visu/", fun, "_visual_", "MOEA/D-DE - No Resource Allocation", ".html"))
+file <- paste0("~/MOEADr/dataExp/visu/", fun, "_visual_", "MOEAD_DE_None", ".html")
+saveWidgetFix(p, file)
 
 p <- visuEvol(moead.norm, "MOEA/D-DE - Norm Resource Allocation", fun, ref1)
-htmlwidgets::saveWidget(p, file = paste0("~/MOEADr/dataExp/visu/", fun, "_visual_", "MOEA/D-DE - Norm Resource Allocation", ".html"))
+file <- paste0("~/MOEADr/dataExp/visu/", fun, "_visual_", "MOEAD_DE_Norm", ".html")
+saveWidgetFix(p, file)
 
 p <- visuEvol(moead.norm.inverse, "MOEA/D-DE - (1-Norm) Resource Allocation", fun, ref1)
-htmlwidgets::saveWidget(p, file = paste0("~/MOEADr/dataExp/visu/", fun, "_visual_", "MOEA/D-DE - (1-Norm) Resource Allocation", ".html"))
+file <- paste0("~/MOEADr/dataExp/visu/", fun, "_visual_", "MOEAD_DE_InverseNorm", ".html")
+saveWidgetFix(p, file)
 
 p <- visuEvol(moead.random, "MOEA/D-DE - Random Resource Allocation", fun, ref1)
-htmlwidgets::saveWidget(p, file = paste0("~/MOEADr/dataExp/visu/", fun, "_visual_", "MOEA/D-DE - Random Resource Allocation", ".html"))
+file <- paste0("~/MOEADr/dataExp/visu/", fun, "_visual_", "MOEAD_DE_Random", ".html")
+saveWidgetFix(p, file)
 
 p <- visuEvol(moead.R.I., "MOEA/D-DE - R.I. Resource Allocation", fun, ref1)
-htmlwidgets::saveWidget(p, file = paste0("~/MOEADr/dataExp/visu/", fun, "_visual_", "MOEA/D-DE - R.I. Resource Allocation", ".html"))
+file <- paste0("~/MOEADr/dataExp/visu/", fun, "_visual_", "MOEAD_DE_RI", ".html")
+saveWidgetFix(p, file)
+
 }
